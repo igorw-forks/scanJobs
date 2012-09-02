e8c68810a8b243247d98762e94ad5605b9e5c2b2
Cal Evans <cal@calevans.com>
2012-09-02 10:15:26 -0500

    Ok, this time I actually added the file in

---
+++ app/ScanJobs/Command/WorkCommand.php
@@ -0,0 +1,36 @@
+<?PHP
+namespace ScanJobs\Command;
+
+use Knp\Command\Command;
+
+class WorkCommand extends Command
+{	
+
+    protected function configure()
+    {
+        $this->setName('work')
+		             ->setDescription('Generic all-purpose work script');
+	}
+
+    protected function execute(\Symfony\Component\Console\Input\InputInterface $input, 
+							   \Symfony\Component\Console\Output\OutputInterface $output)
+	{
+
+		$db     = $this->getSilexApplication()['db'];
+		$sql = 'select * from company';
+		$results = $db->executeQuery($sql)->fetchAll();
+		$apicall = 'http://ec2-107-21-104-179.compute-1.amazonaws.com/v/1/company/%s.js';
+		foreach ($results as $singleCompany) {
+			$company = $singleCompany['company_name'];
+			$company = urlencode($company);
+			$url = sprintf($apicall,$company);
+			$payload = file_get_contents($url);
+			$json = json_decode($payload);
+			print_r($json);
+		}
+		$output->writeln('Done');
+		return;
+    }
+
+
+}
